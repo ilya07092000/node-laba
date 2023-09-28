b035dd75eeccb99ace7e1216bc769a0f704d7ac4
ilya07092000 <ischenko2012@gmail.com>
2023-09-28 18:49:59 +0300

    boolean, null

--- hws/hw13/index.js
+++ hws/hw13/index.js
@@ -46,11 +46,12 @@ function myJSONParse(jsonString) {
   const result = {};
   const quotationMarksRegex = /^"|"$/g;
   const regExp =
-    /((?<key>"\w+"):\s*((?<string>"[^"]+")|(?<number>\d+)|(?<object>\{(?:[^{}]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}(?=,|\s{0,1}}))|(?<array>\[.*?\])))/gi;
+    /((?<key>"\w+"):\s*((?<string>"[^"]+")|(?<number>\d+)|(?<nullValue>\bnull\b)|(?<boolean>\btrue\b|\bfalse\b)|(?<object>\{(?:[^{}]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}(?=,|\s{0,1}}))|(?<array>\[.*?\])))/gi;
   let currentStep;
 
   while ((currentStep = regExp.exec(jsonString))) {
-    let {key, string, number, object, array} = currentStep.groups;
+    let {key, string, number, object, array, boolean, nullValue} =
+      currentStep.groups;
     const formattedKey = key.replace(quotationMarksRegex, ''); // remove quotation marks
     let resultValue = undefined;
 
@@ -62,6 +63,10 @@ function myJSONParse(jsonString) {
       resultValue = +number;
     } else if (string) {
       resultValue = string.replace(quotationMarksRegex, '');
+    } else if (boolean) {
+      resultValue = boolean === 'true';
+    } else if (nullValue) {
+      resultValue = null;
     }
 
     result[formattedKey] = resultValue;
@@ -70,10 +75,10 @@ function myJSONParse(jsonString) {
   return result;
 }
 
-// const jsonString =
-// '{"name":"John Doe","age":30,"is_student":false,"address":{"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1"]}';
-// const jsonString = '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }}';
 const jsonString =
-  '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }, "innerArray": [1, 2, 3, { "name": "test" }], "test": 123}';
+  '{"name":"John Doe","age":30,"is_student":false,"address":{"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1"]}';
+// const jsonString = '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }}';
+// const jsonString =
+// '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }, "innerArray": [1, 2, 3, { "name": "test" }], "test": 123}';
 const jsonObject = myJSONParse(jsonString);
 console.log('jsonObject', jsonObject);
