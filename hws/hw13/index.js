88dae4a42ecef78f6e125668d13bfd61c32dd5b4
ilya07092000 <ischenko2012@gmail.com>
2023-09-30 11:25:03 +0300

    switch case

--- hws/hw13/index.js
+++ hws/hw13/index.js
@@ -1,58 +1,24 @@
-// const originalObject = {
-//   name: 'John Doe',
-//   age: 30,
-//   is_student: false,
-//   address: {
-//     street: '123 Main St',
-//     city: 'Anytown',
-//     state: 'CA',
-//     postal_code: '12345',
-//   },
-//   favorite_numbers: [7, 42, 101],
-//   grades: {
-//     math: 95,
-//     science: 88,
-//     history: 75,
-//   },
-//   contact: null,
-//   testArray: [{key: 'value'}, 1, '1'],
-// };
-// const jsonString =
-//   '{"name":"John Doe","age":30,"is_student":false,"address":{"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1"]}';
-// const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
-// console.log(jsonString);
-
 /**
- * check whether string is json object
- * 1. check brackets at the first and second place
- * 2.
+ * FUNCTIONS TO HANDLE SEPARATE DATA TYPES
  */
-
-// const jsonRegexp = /^{("\w+":\s*("[^"]+"|\d+)(,\s*)?)+}$/gi;
-// const isJsonString = string => {
-//   if (!string) {
-//     return false;
-//   }
-
-//   return jsonRegexp.test(string);
-// };
-// console.log(
-//   isJsonString(
-//     '{"name": "John", "age": 30, "city": "New York", "test": {"other": 123}}',
-//   ),
-// );
-
 const stringRegexp = /"[^"]+"/;
 const numberRegexp = /\d+/;
 const nullValueRegexp = /\bnull\b/;
 const booleanRegexp = /\btrue\b|\bfalse\b/;
 const objectRegexp = /\{(?:[^{}]+|\{(?:[^}{]+|\{[^}{]*\})*\})*\}/;
 const arrayRegexp = /\[.*?\]/;
+const quotationMarksRegex = /^"|"$/g;
+
+/**
+ * MATCH DIFFERENT DATA TYPES INTO SEPARATE GROUPS
+ */
 const defineDataDypeRegexp = new RegExp(
   `(?<object>${objectRegexp.source})|(?<string>${stringRegexp.source})|(?<number>${numberRegexp.source})|(?<nullValue>${nullValueRegexp.source})|(?<boolean>${booleanRegexp.source})|(?<array>${arrayRegexp.source})`,
 );
-const quotationMarksRegex = /^"|"$/g;
 
+/**
+ * JSON PARSER
+ */
 function myJSONParse(jsonString) {
   const result = {};
   const jsonKeyValuesRegexp = new RegExp(
@@ -68,23 +34,57 @@ function myJSONParse(jsonString) {
   return result;
 }
 
+/**
+ * HANDLE ARRAY
+ */
 const handleArray = (inputArray = []) =>
   inputArray.map(item => handleValues(item.match(defineDataDypeRegexp).groups));
 
-const handleValues = ({string, number, object, array, boolean, nullValue}) => {
+/**
+ * HANDLE DIFFERENT VALUES
+ */
+const handleValues = groups => {
+  const ALLOWED_VALUES = [
+    'string',
+    'object',
+    'array',
+    'number',
+    'boolean',
+    'nullValue',
+  ];
+  let valueType;
+  let currValue;
+  Object.entries(groups).forEach(([key, value]) => {
+    if (value !== undefined) {
+      valueType = key;
+      currValue = value;
+    }
+  });
+
+  if (!ALLOWED_VALUES.includes(valueType) || !valueType) {
+    throw new Error(`Can not handle ${currValue}`);
+  }
   let resultValue = undefined;
-  if (object) {
-    resultValue = myJSONParse(object);
-  } else if (array) {
-    resultValue = handleArray(array.replace(/^\[|\]$/, '').split(','));
-  } else if (number) {
-    resultValue = +number;
-  } else if (string) {
-    resultValue = string.replace(quotationMarksRegex, '');
-  } else if (boolean) {
-    resultValue = boolean === 'true';
-  } else if (nullValue) {
-    resultValue = null;
+
+  switch (valueType) {
+    case 'object':
+      resultValue = myJSONParse(currValue);
+      break;
+    case 'array':
+      resultValue = handleArray(currValue.replace(/^\[|\]$/, '').split(','));
+      break;
+    case 'number':
+      resultValue = +currValue;
+      break;
+    case 'string':
+      resultValue = currValue.replace(quotationMarksRegex, '');
+      break;
+    case 'boolean':
+      resultValue = currValue === 'true';
+      break;
+    case 'nullValue':
+      resultValue = null;
+      break;
   }
 
   return resultValue;
@@ -92,8 +92,5 @@ const handleValues = ({string, number, object, array, boolean, nullValue}) => {
 
 const jsonString =
   '{"name":"John Doe","age":30,"is_student":false,"address":{"innerObj":{"innerArray":[1, 2, 3]},"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1", 21]]}';
-// const jsonString = '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }}';
-// const jsonString =
-// '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }, "innerArray": [1, 2, 3, { "name": "test" }], "test": 123}';
 const jsonObject = myJSONParse(jsonString);
 console.log('jsonObject', jsonObject);
