476dc34537afb926d7889894c4b4c55912ad3e9c
ilya07092000 <ischenko2012@gmail.com>
2023-09-30 11:08:19 +0300

    handleValues

--- hws/hw13/index.js
+++ hws/hw13/index.js
@@ -51,44 +51,47 @@ const arrayRegexp = /\[.*?\]/;
 const defineDataDypeRegexp = new RegExp(
   `(?<object>${objectRegexp.source})|(?<string>${stringRegexp.source})|(?<number>${numberRegexp.source})|(?<nullValue>${nullValueRegexp.source})|(?<boolean>${booleanRegexp.source})|(?<array>${arrayRegexp.source})`,
 );
+const quotationMarksRegex = /^"|"$/g;
 
 function myJSONParse(jsonString) {
   const result = {};
-  const quotationMarksRegex = /^"|"$/g;
   const jsonKeyValuesRegexp = new RegExp(
     `((?<key>"\\w+"):\\s*(${defineDataDypeRegexp.source}))`,
     'gi',
   );
   let currentStep;
-
   while ((currentStep = jsonKeyValuesRegexp.exec(jsonString))) {
-    let {key, string, number, object, array, boolean, nullValue} =
-      currentStep.groups;
+    let {key, ...groups} = currentStep.groups;
     const formattedKey = key.replace(quotationMarksRegex, ''); // remove quotation marks
-    let resultValue = undefined;
-
-    if (object) {
-      resultValue = myJSONParse(object);
-    } else if (array) {
-      resultValue = array.replace(/^\[|\]$/g, '').split(',');
-    } else if (number) {
-      resultValue = +number;
-    } else if (string) {
-      resultValue = string.replace(quotationMarksRegex, '');
-    } else if (boolean) {
-      resultValue = boolean === 'true';
-    } else if (nullValue) {
-      resultValue = null;
-    }
-
-    result[formattedKey] = resultValue;
+    result[formattedKey] = handleValues(groups);
   }
-
   return result;
 }
 
+const handleArray = (inputArray = []) =>
+  inputArray.map(item => handleValues(item.match(defineDataDypeRegexp).groups));
+
+const handleValues = ({string, number, object, array, boolean, nullValue}) => {
+  let resultValue = undefined;
+  if (object) {
+    resultValue = myJSONParse(object);
+  } else if (array) {
+    resultValue = handleArray(array.replace(/^\[|\]$/, '').split(','));
+  } else if (number) {
+    resultValue = +number;
+  } else if (string) {
+    resultValue = string.replace(quotationMarksRegex, '');
+  } else if (boolean) {
+    resultValue = boolean === 'true';
+  } else if (nullValue) {
+    resultValue = null;
+  }
+
+  return resultValue;
+};
+
 const jsonString =
-  '{"name":"John Doe","age":30,"is_student":false,"address":{"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1"]}';
+  '{"name":"John Doe","age":30,"is_student":false,"address":{"innerObj":{"innerArray":[1, 2, 3]},"street":"123 Main St","city":"Anytown","state":"CA","postal_code":"12345"},"favorite_numbers":[7,42,101],"grades":{"math":95,"science":88,"history":75},"contact":null,"testArray":[{"key":"value"},1,"1", 21]]}';
 // const jsonString = '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }}';
 // const jsonString =
 // '{"name": "John", "age": 30, "city": "New York", "innerObj": { "nameInner": "test", "otherTest": { "value2": 123 } }, "innerArray": [1, 2, 3, { "name": "test" }], "test": 123}';
